7cb0779b63c2f3f90f06a8f1cb5de3a734525a90
ChengZhao <orange94260@gmail.com>
2021-01-26 15:09:16 +0800

    Add Texas Hold'em.

--- env/__init__.py
+++ env/__init__.py
@@ -2,8 +2,10 @@ import env.tiger.tiger
 import env.rock_sample.rock_sample
 import env.kuhn_poker.kuhn_poker
 import env.leduc_poker.leduc_poker
+import env.texas_holdem.texas_holdem
 
 Tiger = env.tiger.tiger.Tiger
 RockSample = env.rock_sample.rock_sample.RockSample
 KuhnPoker = env.kuhn_poker.kuhn_poker.KuhnPoker
 LeducPoker = env.leduc_poker.leduc_poker.LeducPoker
+TexasHoldem = env.texas_holdem.texas_holdem.TexasHoldem
